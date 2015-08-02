0cd9885d6f2f6861eb56250cf5b64e313486f438
Kyle McGrogan <kyle.mcgrogan@dvinci.com>
2015-08-02 18:54:58 -0400

    Skills added poorly

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.default')
 @section('css')
     <style>
+        .name {
+            font-size:56px;
+        }
         .center {
             text-align: center;
         }
         .content-section {
             padding-top:40px;
         }
-        .work {
-            font-size: 30px;
-
-        }
-        .skills {
-            font-size: 30px;
+        .section-header {
+            font-size: 40px;
+            display:table;
+            margin:0 auto;
         }
         .job {
             padding-left:15px;
@@ -34,17 +35,23 @@
         .info {
             padding-left: 20px;
         }
+
+        ul
+        {
+            list-style-type: none;
+        }
     </style>
 @stop
 
 @section('content')
-    <h1 class="center">Kyle McGrogan</h1>
+    <h1 class="name center">Kyle McGrogan</h1>
 
     <div class="content-section">
-        <span class="work">Work Experience</span>
-        <a class="btn btn-sm btn-default" role="button" data-toggle="collapse" href="#work" aria-expanded="true" aria-controls="work">
-            Hide/Show
-        </a>
+        <span class="section-header">Work Experience
+            <a class="btn btn-sm btn-default center" role="button" data-toggle="collapse" href="#work" aria-expanded="true" aria-controls="work">
+                Hide/Show
+            </a>
+        </span>
         <br/>
         <div id="work" class="collapse in">
 
@@ -116,24 +123,62 @@
         </div>
     </div>
 
-    {{--<div class="content-section">--}}
-        {{--<span class="skills">Skills</span>--}}
-        {{--<a class="btn btn-sm btn-default" role="button" data-toggle="collapse" href="#knowledge" aria-expanded="true" aria-controls="knowledge">--}}
-            {{--Hide/Show--}}
-        {{--</a>--}}
-        {{--<br/>--}}
-        {{--<div id="skills" class="collapse in">--}}
-            {{--<div class="job">--}}
-                {{--<div class="title">Web Developer</div>--}}
-                {{--<div class="employer">d'Vinci Interactive</div>--}}
-                {{--<div class="dates">--}}
-                    {{--October 2014 - Current--}}
-                {{--</div>--}}
-                {{--<div class="info">--}}
-                    {{--Developing PHP solutions to satisfy client needs, including client API interactions.--}}
-                {{--</div>--}}
-            {{--</div>--}}
+    <div class="content-section">
+        <span class="section-header">Skills
+            <a class="btn btn-sm btn-default" role="button" data-toggle="collapse" href="#knowledge" aria-expanded="true" aria-controls="knowledge">
+                Hide/Show
+            </a>
+        </span>
+        <br/>
+        <div id="skills" class="collapse in">
+            <ul>
+                <li>
+                    <span class="title">Languages/Markup:</span>
+                    <ul>
+                        <li>PHP (Zend 2, CodeIgniter, Laravel)</li>
+                        <li>Javascript (jQuery, AngularJS, nodeJS)</li>
+                        <li>MySQL</li>
+                        <li>PostgreSQL</li>
+                        <li>Java EE</li>
+                        <li>.NET</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>XML</li>
+                        <li>XML</li>
+                        <li>Apache Velocity</li>
+                        <li>Apache ANT Including Apache Ivy</li>
+                        <li>Java ServerPages</li>
+                        <li>PrimeFaces</li>
+                        <li>Java Spring Framework</li>
+                    </ul>
+                </li>
+                <li>
+                    <span class="title">Operating Systems:</span>
+                    <ul>
+                        <li>Most Windows Operating Systems</li>
+                        <li>Red Hat Linux</li>
+                        <li>Mac OS X Mavericks 10.9</li>
+                    </ul>
+                </li>
+                <li>
+                    <span class="title">Applications/Other:</span>
+                    <ul>
+                        <li>Eclipse IDE ( + Spring Tool Suite)</li>
+                        <li>Visual Studio</li>
+                        <li>NetBeans</li>
+                        <li>PHPStorm</li>
+                        <li>Most Major Browsers</li>
+                        <li>MS Office Suite</li>
+                        <li>Libre Office</li>
+                        <li>VirtualBox/VMWare</li>
+                        <li>IBM WebSphere</li>
+                        <li>Apache Subversion</li>
+                        <li>Git</li>
+                    </ul>
+                </li>
+            </ul>
+            </ul>
 
-        {{--</div>--}}
-    {{--</div>--}}
+        </div>
+    </div>
 @stop
