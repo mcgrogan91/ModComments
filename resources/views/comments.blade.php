c278b70ef927573a30017fe5cbaee797b3d89c10
Kyle McGrogan <kyle.mcgrogan@dvinci.com>
2016-02-26 12:35:14 -0500

    Fixing display issue
    'm' in the date formatting was outputting the month.  It should have been 'i' for minutes.

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -1,6 +1,6 @@
 <?php require_once(base_path().'/vendor/iamcal/lib_autolink/lib_autolink.php'); ?>
 <div id='comments' style='padding-bottom:15px;'>
   @foreach($comments as $comment)
-   {{ date('M d Y,  H:m:s', strtotime($comment['created_at'])) }}: {!! autolink(e($comment['comment_text']), 100) !!} - {{ $comment['mod_name'] }} <br/>
+   {{ date('M d Y,  H:i:s', strtotime($comment['created_at'])) }}: {!! autolink(e($comment['comment_text']), 100) !!} - {{ $comment['mod_name'] }} <br/>
   @endforeach
 </div>
